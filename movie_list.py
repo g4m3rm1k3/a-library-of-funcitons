ea7a04ed495686c5be744931bb793c35255b3c4f
Sagittariusa <jwagner.pro@gmail.com>
2020-08-22 20:55:01 +0000

    add .env file to store Environmental variables and secrets add r.status_code check to catch an error if connection to the service is non-200

--- movie_list.py
+++ movie_list.py
@@ -1,12 +1,20 @@
-import requests   
+import requests
+import os
 from bs4 import BeautifulSoup
+from dotenv import load_dotenv
 from lxml import html
 from cssselect import GenericTranslator, SelectorError
 
-r = requests.get('https://www.imdb.com/list/ls055592025/')
-tree = html.fromstring(r.content)
-movie = tree.xpath('//h3[@class="lister-item-header"]//a[starts-with(@href, "/")]/text()')
-year = tree.xpath('//h3[@class="lister-item-header"]//span[contains(@class, "lister-item-year")]/text()')
-placement = tree.xpath('//h3[@class="lister-item-header"]//span[contains(@class,"text-primary")]/text()')
+load_dotenv()
+IMDB_URL = os.getenv('IMDB_URL')
+
+r = requests.get(IMDB_URL)
+if r.status_code == 200:
+	tree = html.fromstring(r.content)
+	movie = tree.xpath('//h3[@class="lister-item-header"]//a[starts-with(@href, "/")]/text()')
+	year = tree.xpath('//h3[@class="lister-item-header"]//span[contains(@class, "lister-item-year")]/text()')
+	placement = tree.xpath('//h3[@class="lister-item-header"]//span[contains(@class,"text-primary")]/text()')
+else:
+	print(f'There was an error connecting to the URL. Status code: {r.status_code}')
 
 
